01d80a0f95e0eddb5b06d89a766cb5e168b1b75a
Alaa ELHamouly <AElHamouly@sure.com.sa>
2025-05-21 21:21:46 +0300

    prepare for deplotment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -7,9 +8,17 @@ const SCAN_LIMIT = 3;
 const FILE = 'scan-count.json';
 const FORM_URL = 'https://docs.google.com/forms/d/18LqL7Ed3v1ynObPUEtuOEFjwyuziaYmxTeJ90PNCoXg';
 
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
 if (!fs.existsSync(FILE)) {
   fs.writeFileSync(FILE, JSON.stringify({ count: 0 }));
 }
+
+// Route to serve QR code page
+app.get('/qr', (req, res) => {
+  res.sendFile(path.join(__dirname, 'qr.html'));
+});
 
 app.get('/', (req, res) => {
   const data = JSON.parse(fs.readFileSync(FILE));
